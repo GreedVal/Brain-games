11e797d7f83d4218303334ef07412ef9e803ab5f
GreedVal <137034289+GreedVal@users.noreply.github.com>
2024-09-16 19:40:00 +0300

    greedval

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/GreedVal/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/GreedVal/php-project-45/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/a9c0227cd7c803fd9223/maintainability)](https://codeclimate.com/github/GreedVal/php-project-45/maintainability)
+
+Первый проект из четырёх, в рамках профессии PHP-программист на [Хекслет](https://ru.hexlet.io/professions/php).
+
+### Описание проекта
+Необходимо реализовать набор мини-игр, запускаемых из консоли.
+### Установка
+Для глобальной установки выполните команду:
+`$ composer global require greedval/hexlet-project-45`
--- src/Engine.php
+++ src/Engine.php
@@ -28,30 +28,15 @@ function getRandomSign(array $signs): string
 
 function getDescription(string $key = ''): string
 {
-    $description = '';
+    $descriptions = [
+        'even' => 'Answer "yes" if the number is even, otherwise answer "no".',
+        'calc' => 'What is the result of the expression?',
+        'gcd' => 'Find the greatest common divisor of given numbers.',
+        'progression' => 'What number is missing in the progression?',
+        'prime' => 'Answer "yes" if given number is prime. Otherwise answer "no".',
+    ];
 
-    switch ($key) {
-        case 'even':
-            $description = 'Answer "yes" if the number is even, otherwise answer "no".';
-            break;
-        case 'calc':
-            $description = 'What is the result of the expression?';
-            break;
-        case 'gcd':
-            $description = 'Find the greatest common divisor of given numbers.';
-            break;
-        case 'progression':
-            $description = 'What number is missing in the progression?';
-            break;
-        case 'prime':
-            $description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-            break;
-        default:
-            $description = 'No description game';
-            break;
-    }
-
-    return $description;
+    return $descriptions[$key] ?? 'No description game';
 }
 
 function processGame(string $gameRule, array $questionsAndAnswers): void
