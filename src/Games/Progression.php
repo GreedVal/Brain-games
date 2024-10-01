813fb536987458c88b7a48982ad460ba115cd93b
GreedVal <137034289+GreedVal@users.noreply.github.com>
2024-10-01 18:39:35 +0300

    greedval

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -5,8 +5,8 @@ namespace Hexlet\Code\Games\Progression;
 use Hexlet\Code\Engine;
 
 const LONG_PROGRESSION = 10;
-const MIN_POSITION = 1;
-const MAX_POSITION = 10;
+const MIN_POSITION = 0;
+const MAX_POSITION = 9;
 const MIN_STEP = 2;
 const MAX_STEP = 5;
 const DESCRIPTION = 'What number is missing in the progression?';
@@ -16,6 +16,7 @@ function generateProgression(int $step, int $long, int $start): array
 {
     $end = $start + $step * ($long - 1);
     return range($start, $end, $step);
+
 }
 
 function run(): void
